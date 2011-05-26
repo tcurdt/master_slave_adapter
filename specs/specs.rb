8b6f95f6d928dd9e030c89b075860f3d23a16256
Torsten Curdt <tcurdt@vafer.org>
2011-05-26 14:05:20 +0200

    more specs

--- specs/specs.rb
+++ specs/specs.rb
@@ -237,40 +237,27 @@ describe ActiveRecord::ConnectionAdapters::MasterSlaveAdapter do
       ActiveRecord::ConnectionAdapters::MasterSlaveAdapter::Clock.new('', pos)
     end
 
-    def slave_position(pos)
+    def slave_should_report_clock(pos)
       if pos.instance_of? Fixnum
         pos = [ pos ]
       end
-      values = pos.map { |p| { 'Master_Log_File' => '', 'Position' => p } }
+      values = pos.map { |p| { 'Master_Log_File' => '', 'Read_Master_Log_Pos' => p } }
       @slave_connection.should_receive('select_one').exactly(pos.length).with('SHOW SLAVE STATUS').and_return(*values)
     end
 
-    it "should properly handle cascading with_consistency blocks" do
-      ActiveRecord::ConnectionAdapters::MasterSlaveAdapter.reset!
-
-      method = 'select_one'
-
-      slave_position([ zero, zero, zero ])
-      @slave_connection.should_receive(method).exactly(3).times.with('testing').and_return(true)
-
-      old_clock = zero
-      new_clock = ActiveRecord::Base.with_consistency(old_clock) do
-        ActiveRecord::Base.connection.send(method, 'testing')
-
-        ActiveRecord::Base.with_consistency(old_clock) do
-          ActiveRecord::Base.connection.send(method, 'testing')
-        end
-
-        ActiveRecord::Base.connection.send(method, 'testing')
+    def master_should_report_clock(pos)
+      if pos.instance_of? Fixnum
+        pos = [ pos ]
       end
-      new_clock.should equal(old_clock)
+      values = pos.map { |p| { 'File' => '', 'Position' => p } }
+      @master_connection.should_receive('select_one').exactly(pos.length).with('SHOW MASTER STATUS').and_return(*values)
     end
 
     ActiveRecord::ConnectionAdapters::MasterSlaveAdapter::SELECT_METHODS.each do |method|
 
       it "should send the method '#{method}' to the slave if no clock is given" do
         ActiveRecord::ConnectionAdapters::MasterSlaveAdapter.reset!
-        slave_position(0)
+        slave_should_report_clock(0)
         @slave_connection.should_receive(method).with('testing').and_return(true)
         old_clock = nil
         new_clock = ActiveRecord::Base.with_consistency(old_clock) do
@@ -282,11 +269,11 @@ describe ActiveRecord::ConnectionAdapters::MasterSlaveAdapter do
 
       it "should send the method '#{method}' to the master if slave hasn't cought up to required clock yet" do
         ActiveRecord::ConnectionAdapters::MasterSlaveAdapter.reset!
-        slave_position(0)
+        slave_should_report_clock(0)
         @master_connection.should_receive(method).with('testing').and_return(true)
         old_clock = master_position(1)
         new_clock = ActiveRecord::Base.with_consistency(old_clock) do
-          ActiveRecord::Base.connection.send( method, 'testing' )
+          ActiveRecord::Base.connection.send(method, 'testing' )
         end
         new_clock.should be_a(ActiveRecord::ConnectionAdapters::MasterSlaveAdapter::Clock)
         new_clock.should equal(old_clock)
@@ -296,7 +283,7 @@ describe ActiveRecord::ConnectionAdapters::MasterSlaveAdapter do
         ActiveRecord::ConnectionAdapters::MasterSlaveAdapter.reset!
         @master_connection.stub!(:open_transactions).and_return(1)
         @master_connection.should_receive(method).with('testing').and_return(true)
-        old_clock = nil
+        old_clock = zero
         new_clock = ActiveRecord::Base.with_consistency(old_clock) do
           ActiveRecord::Base.connection.send(method, 'testing')
         end
@@ -304,6 +291,73 @@ describe ActiveRecord::ConnectionAdapters::MasterSlaveAdapter do
         new_clock.should equal(ActiveRecord::ConnectionAdapters::MasterSlaveAdapter::Clock.zero)
       end
 
+      it "should send the method '#{method}' to the master after a write operation" do
+        ActiveRecord::ConnectionAdapters::MasterSlaveAdapter.reset!
+        slave_should_report_clock(0)
+        master_should_report_clock(2)
+        @slave_connection.should_receive(method).with('testing').and_return(true)
+        @master_connection.should_receive('update').with('testing').and_return(true)
+        @master_connection.should_receive(method).with('testing').and_return(true)
+        old_clock = zero
+        new_clock = ActiveRecord::Base.with_consistency(old_clock) do
+          ActiveRecord::Base.connection.send(method, 'testing')
+          ActiveRecord::Base.connection.send('update', 'testing')
+          ActiveRecord::Base.connection.send(method, 'testing')
+        end
+        new_clock.should be_a(ActiveRecord::ConnectionAdapters::MasterSlaveAdapter::Clock)
+        new_clock.should > old_clock
+      end
+
+    end
+
+    it "should work in simple cascades" do
+      ActiveRecord::ConnectionAdapters::MasterSlaveAdapter.reset!
+      slave_should_report_clock([ 0, 0, 0 ])
+      @slave_connection.should_receive('select_one').exactly(3).times.with('testing').and_return(true)
+      old_clock = zero
+      new_clock = ActiveRecord::Base.with_consistency(old_clock) do
+        ActiveRecord::Base.connection.send('select_one', 'testing')
+        ActiveRecord::Base.with_consistency(old_clock) do
+          ActiveRecord::Base.connection.send('select_one', 'testing')
+        end
+        ActiveRecord::Base.connection.send('select_one', 'testing')
+      end
+      new_clock.should equal(old_clock)
+    end
+
+    it "should pass on the master connection and clock in the cascades" do
+      ActiveRecord::ConnectionAdapters::MasterSlaveAdapter.reset!
+      slave_should_report_clock([ 0, 0, 0 ])
+      master_should_report_clock(1)
+      @slave_connection.should_receive('select_all').exactly(1).times.with('testing').and_return(true)
+      @master_connection.should_receive('update').once.with('testing').and_return(true)
+      @master_connection.should_receive('select_all').exactly(2).times.with('testing').and_return(true)
+      old_clock = zero
+      new_clock = ActiveRecord::Base.with_consistency(old_clock) do
+        Thread.current[:master_slave_select_connection].should == nil
+        ActiveRecord::Base.connection.send('select_all', 'testing')
+        Thread.current[:master_slave_clock].should equal(old_clock)
+        Thread.current[:master_slave_select_connection].should == :slave
+
+        ActiveRecord::Base.connection.send('update', 'testing')
+        Thread.current[:master_slave_clock].should > old_clock
+        Thread.current[:master_slave_select_connection].should == :master
+
+        ActiveRecord::Base.with_consistency(old_clock) do
+          Thread.current[:master_slave_select_connection].should == nil
+          Thread.current[:master_slave_clock].should > old_clock
+          ActiveRecord::Base.connection.send('select_all', 'testing')
+          Thread.current[:master_slave_select_connection].should == :master
+          Thread.current[:master_slave_clock].should > old_clock
+        end
+              
+        Thread.current[:master_slave_select_connection].should == nil
+        Thread.current[:master_slave_clock].should > old_clock
+        ActiveRecord::Base.connection.send('select_one', 'testing')
+        Thread.current[:master_slave_select_connection].should == :master
+      end
+      Thread.current[:master_slave_select_connection].should == nil
+      new_clock.should > old_clock
     end
 
   end

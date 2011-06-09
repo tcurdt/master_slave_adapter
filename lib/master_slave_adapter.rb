0fdbb3b0905d394ac85d1d490f54171a936a896a
Tobias Schmidt <tobi@gronom.de>
2011-06-09 23:18:07 +0200

    reduce require calls

--- lib/master_slave_adapter.rb
+++ lib/master_slave_adapter.rb
@@ -1,7 +1,5 @@
 require 'active_record/connection_adapters/master_slave_adapter'
 
-require 'master_slave_adapter/active_record_extensions'
-require 'master_slave_adapter/adapter'
-require 'master_slave_adapter/instance_methods_generation'
-
-require 'master_slave_adapter/version'
+module MasterSlaveAdapter
+  autoload :Version, 'master_slave_adapter/version'
+end

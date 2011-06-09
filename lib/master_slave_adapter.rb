41014418a99baa0eded88c49a9c51ea37f32fa1c
Tobias Schmidt <tobi@gronom.de>
2011-06-09 13:28:08 -0700

    fix typo which prevents loading active_record_extensions

--- lib/master_slave_adapter.rb
+++ lib/master_slave_adapter.rb
@@ -1,6 +1,6 @@
 require 'active_record/connection_adapters/master_slave_adapter'
 
-require 'master_slave_adapter/active_record_extension'
+require 'master_slave_adapter/active_record_extensions'
 require 'master_slave_adapter/adapter'
 require 'master_slave_adapter/instance_methods_generation'
 

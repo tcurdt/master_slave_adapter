0b3fc6ecc707a5ffe2c89a9f19ec0260172620cb
Brian Leonard <brian@bleonard.com>
2012-01-21 19:29:27 -0800

    Allows rake db:create and db:migrate
    db:create passes in nil for the database connection and
    db:migrate uses the idnexes method
    (cherry picked from commit 9cd85f86d11b7449067ca758afed56877eb2c58a)

--- lib/master_slave_adapter.rb
+++ lib/master_slave_adapter.rb
@@ -55,6 +55,7 @@ module ActiveRecord
           reject { |k,_| skip.include?(k) }.
           merge(:adapter => config.fetch(:connection_adapter))
         ([config.fetch(:master)] + config.fetch(:slaves, [])).map do |cfg|
+          cfg[:database] = defaults[:database] if defaults.has_key?(:database)
           cfg.symbolize_keys!.reverse_merge!(defaults)
         end
         config
@@ -273,6 +274,7 @@ module ActiveRecord
                :to => :master_connection }])
       # no clear interface contract:
       delegate :tables,         # commented in SchemaStatements
+               :indexes,        # migrations
                :truncate_table, # monkeypatching database_cleaner gem
                :primary_key,    # is Base#primary_key meant to be the contract?
                :to => :master_connection
